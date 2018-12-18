61885b4f8b5c6e2683e0708d6ed4716dfd5f007d
coolyangzc <yang-zc17@mails.tsinghua.edu.cn>
2018-12-18 11:27:16 +0800

    Data record

--- ProximateSpeechRecorder/app/src/main/java/com/yzc/proximatespeechrecorder/MainActivity.java
+++ ProximateSpeechRecorder/app/src/main/java/com/yzc/proximatespeechrecorder/MainActivity.java
@@ -1,17 +1,41 @@
 package com.yzc.proximatespeechrecorder;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
+
         Intent intent = new Intent();
         intent.setClass(this, SensorActivity.class);
         startActivity(intent);
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
     }
 }
--- ProximateSpeechRecorder/app/src/main/java/com/yzc/proximatespeechrecorder/SensorActivity.java
+++ ProximateSpeechRecorder/app/src/main/java/com/yzc/proximatespeechrecorder/SensorActivity.java
@@ -5,20 +5,32 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 
-public class SensorActivity extends Activity implements SensorEventListener {
+public class SensorActivity extends Activity implements SensorEventListener, View.OnClickListener {
+
+    private Long startTimestamp = 0L;
+    private Boolean isRecording = false;
 
     private SensorManager mSensorManager;
-    private Sensor sensor;
     private TextView textView_sensor;
+    private Button button_record;
     private int sensorType[] = {
             Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_ROTATION_VECTOR,
             Sensor.TYPE_GYROSCOPE, Sensor.TYPE_MAGNETIC_FIELD,
@@ -33,11 +45,16 @@ public class SensorActivity extends Activity implements SensorEventListener {
     };
     private float sensorData[][] = new float[sensorType.length][];
 
+    private final String pathName = Environment.getExternalStorageDirectory().getPath() + "/SensorData/";
+    private FileOutputStream fos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
         textView_sensor = findViewById(R.id.textView_sensor);
+        button_record = findViewById(R.id.button_record);
+        button_record.setOnClickListener(this);
         loadSensor();
     }
 
@@ -57,10 +74,14 @@ public class SensorActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         float[] values = sensorEvent.values;
+        String s = "";
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < sensorType.length; i++) {
-            if (sensorEvent.sensor.getType() == sensorType[i])
+            if (sensorEvent.sensor.getType() == sensorType[i]) {
                 sensorData[i] = values;
+                s = sensorName[i];
+            }
+
             if (sensorData[i] != null) {
                 sb.append(sensorName[i]);
                 for (float data : sensorData[i])
@@ -69,6 +90,30 @@ public class SensorActivity extends Activity implements SensorEventListener {
             }
         }
         textView_sensor.setText(sb.toString());
+        if (!isRecording)
+            return;
+        if (startTimestamp == 0) {
+            startTimestamp = sensorEvent.timestamp;
+            String ss = Long.toString(System.currentTimeMillis()) + "\n";
+            ss += Long.toString(SystemClock.elapsedRealtimeNanos()) + "\n";
+            try {
+                fos.write(ss.getBytes());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        s += " " + Long.toString((sensorEvent.timestamp - startTimestamp) / 1000000);
+        s += " " + Integer.toString(sensorEvent.accuracy);
+        for (int i=0; i < sensorEvent.values.length; ++i)
+            s += " " + Float.toString(sensorEvent.values[i]);
+        s += "\n";
+        byte [] buffer = s.getBytes();
+        try {
+            fos.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -84,12 +129,47 @@ public class SensorActivity extends Activity implements SensorEventListener {
         }
 
         for(int type:sensorType) {
-            sensor = mSensorManager.getDefaultSensor(type);
+            Sensor sensor = mSensorManager.getDefaultSensor(type);
             mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
+
         List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         for (Sensor sensor : sensorList) {
             Log.d("SensorList", sensor.getName());
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_record:
+                isRecording ^= true;
+                if (isRecording) {
+                    button_record.setText("Stop Recording");
+                    try {
+                        SimpleDateFormat format = new SimpleDateFormat("yy.MM.dd HH_mm_ss", Locale.US);
+                        String fileName = format.format(new Date()) + ".txt";
+                        File path = new File(pathName);
+                        File file = new File(pathName + fileName);
+                        boolean res;
+                        if (!path.exists())
+                            res = path.mkdir();
+                        if (!file.exists())
+                            res = file.createNewFile();
+                        fos = new FileOutputStream(file);
+                        MediaScannerConnection.scanFile(this, new String[] { file.getAbsolutePath() }, null, null);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    button_record.setText("Start Recording");
+                    startTimestamp = 0L;
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
         }
     }
 }
