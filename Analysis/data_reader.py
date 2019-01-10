2f7b24a2858ef1dbc5dc166854812bdb36781481
coolyangzc <yang-zc17@mails.tsinghua.edu.cn>
2019-01-10 16:33:24 +0800

    Implement pyplot

--- Analysis/data_reader.py
+++ Analysis/data_reader.py
@@ -1,17 +1,20 @@
 import re
 
 
-class Frame:
+class FrameList:
     def __init__(self):
         self.type = ""
-        self.time_stamp, self.accuracy = 0, 0
+        self.time_stamp, self.accuracy = [], []
         self.value = []
+
+    def get_data_len(self):
+        return len(self.value)
 
 
 class Data:
 
     start_time_millis, start_up_time_millis, start_elapsed_realtime_nanos = 0, 0, 0
-    frame_list = {}
+    type_to_list = {}
 
     def read(self, file_path, show_msg = True):
         f = open(file_path, "r")
@@ -22,18 +25,25 @@ class Data:
         self.start_up_time_millis = int(lines[1])
         self.start_elapsed_realtime_nanos = int(lines[2])
         for line in lines[3:]:
-            f = line_to_frame(line)
-            if f.type not in self.frame_list:
-                self.frame_list.setdefault(f.type, [])
-            self.frame_list[f.type].append(f)
+            self.__add_frame(line)
 
+    def __add_frame(self, line):
+        data = line.split()
+        type_name = data[0]
+        if type_name not in self.type_to_list:
+            self.type_to_list.setdefault(type_name, FrameList())
+            frame_list = self.type_to_list[type_name]
+            frame_list.type = type_name
 
-def line_to_frame(line):
-    f = Frame()
-    data = line.split()
-    f.type = data[0]
-    f.time_stamp = int(data[1])
-    f.accuracy = int(data[2])
-    for v in data[3:]:
-        f.value.append(float(v))
-    return f
+        frame_list = self.type_to_list[type_name]
+        frame_list.time_stamp.append(int(data[1]))
+        frame_list.accuracy.append(int(data[2]))
+
+        for i in range(len(data) - 3):
+            if i >= len(frame_list.value):
+                frame_list.value.append([])
+            frame_list.value[i].append(float(data[i+3]))
+
+    def get_list(self, type_name) -> 'FrameList':
+        if type_name in self.type_to_list:
+            return self.type_to_list[type_name]
--- Analysis/data_visualization.py
+++ Analysis/data_visualization.py
@@ -1,12 +1,14 @@
 import data_reader
+from data_reader import FrameList
+import matplotlib.pyplot as plt
 
 
 data = data_reader.Data()
 data.read("../Data/181228 17_08_01.txt")
 
-for d in data.frame_list['ACCELEROMETER']:
-    print(d.time_stamp)
+acc_list = data.get_list('ACCELEROMETER')
 
-
-
-
+plt.figure(1)
+for i in range(acc_list.get_data_len()):
+    plt.plot(acc_list.time_stamp, acc_list.value[i])
+plt.show()
