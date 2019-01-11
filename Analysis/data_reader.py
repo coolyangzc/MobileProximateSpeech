dfd552d6b239fcf999cb85c9f8c26b07633e3ccf
coolyangzc <yang-zc17@mails.tsinghua.edu.cn>
2019-01-11 14:10:21 +0800

    Data visualization of all sensors

--- Analysis/data_reader.py
+++ Analysis/data_reader.py
@@ -1,5 +1,3 @@
-import re
-
 
 class FrameList:
     def __init__(self):
@@ -14,9 +12,10 @@ class FrameList:
 class Data:
 
     start_time_millis, start_up_time_millis, start_elapsed_realtime_nanos = 0, 0, 0
+    min_time, max_time = 0, 0
     type_to_list = {}
 
-    def read(self, file_path, show_msg = True):
+    def read(self, file_path, show_msg=True):
         f = open(file_path, "r")
         lines = f.readlines()
         f.close()
@@ -37,6 +36,7 @@ class Data:
 
         frame_list = self.type_to_list[type_name]
         frame_list.time_stamp.append(int(data[1]))
+        self.max_time = max(int(data[1]), self.max_time)
         frame_list.accuracy.append(int(data[2]))
 
         for i in range(len(data) - 3):
@@ -44,6 +44,15 @@ class Data:
                 frame_list.value.append([])
             frame_list.value[i].append(float(data[i+3]))
 
+    def get_types(self):
+        type_list = []
+        for t in self.type_to_list:
+            type_list.append(t)
+        return type_list
+
     def get_list(self, type_name) -> 'FrameList':
         if type_name in self.type_to_list:
             return self.type_to_list[type_name]
+
+    def get_max_time(self):
+        return self.max_time
--- Analysis/data_visualization.py
+++ Analysis/data_visualization.py
@@ -1,5 +1,4 @@
 import data_reader
-from data_reader import FrameList
 import matplotlib.pyplot as plt
 
 
@@ -7,8 +6,27 @@ data = data_reader.Data()
 data.read("../Data/181228 17_08_01.txt")
 
 acc_list = data.get_list('ACCELEROMETER')
+type_list = data.get_types()
+type_list.remove('TOUCH')
+type_list.remove('CAPACITY')
+type_list.sort()
 
-plt.figure(1)
-for i in range(acc_list.get_data_len()):
-    plt.plot(acc_list.time_stamp, acc_list.value[i])
+n = 0
+
+print(data.get_max_time())
+plt.figure(figsize=(10, 40))
+for t in type_list:
+    if t == 'TOUCH' or t == 'CAPACITY':
+        continue
+    n += 1
+    plt.subplot(len(type_list), 1, n)
+    plt.xlim(0, data.get_max_time())
+    frame_list = data.get_list(t)
+
+    plt.title(t)
+    for i in range(frame_list.get_data_len()):
+        plt.plot(frame_list.time_stamp, frame_list.value[i], label=i)
+    plt.legend()
+
 plt.show()
+
