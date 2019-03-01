ebe607423e3c46cb737acb8dc438d5fff21a10b3
郑逢时 <962907278@qq.com>
2019-03-01 13:29:30 +0800

    [update] refine RNN's parameters.

--- Analysis/voice/RNN-classifier.py
+++ Analysis/voice/RNN-classifier.py
@@ -3,6 +3,8 @@ from keras import optimizers
 from keras.models import Sequential
 import numpy as np
 import os
+from utils.logger import DualLogger
+from utils.tools import date_time
 
 import utils.voice_data_loader as loader
 from utils.voice_data_loader import show_shape, DataPack
@@ -13,19 +15,17 @@ def build_model():
 	print('building model...')
 	layer_units = (40, 100, 1)
 	model = Sequential()
-	model.add(layers.SimpleRNN(units=layer_units[1], return_sequences=False, input_shape=(None, layer_units[0])))
+	model.add(layers.GRU(units=layer_units[1], return_sequences=False, input_shape=(None, layer_units[0])))
+	model.add(layers.Dropout(0.2))
 	model.add(layers.Dense(units=layer_units[2], activation='sigmoid'))
-	# model.add(layers.Dense(units=100, activation='relu', input_shape=(360,)))
-	# model.add(layers.Dense(units=10, activation='relu'))
-	# model.add(layers.Dense(units=1, activation='sigmoid'))
 
-	sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+	sgd = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
 	model.compile(optimizer=sgd, loss='mse', metrics=['acc'])
 	print('built.')
 	return model
 
 
-def load_train_test(wkdir):
+def load_train_test(wkdir, test_size):
 	dataset = loader.load_ftr_from_dir(wkdir)
 	dataset = loader.apply_subsampling(*dataset, **subsampling_config)
 	# reshape to (n_units, n_frame, n_mfcc)
@@ -34,15 +34,25 @@ def load_train_test(wkdir):
 	print('data shape like:')
 	show_shape(dataset.data)
 	print('data loaded.\n')
-	return loader.train_test_split(*dataset, test_size=0.1)
+	return loader.train_test_split(*dataset, test_size=test_size)
 
 
 if __name__ == '__main__':
 	os.chdir('..')
-	wkdir = 'Data/Sounds/yzc/'
-	train, test = load_train_test(wkdir)
+	os.path.exists('model_state')
+	date_time = date_time()
+	DualLogger('logs/%sRNN.txt' % date_time)
+	print(os.getcwd())
+	wkdir = 'Data/Sounds/yzc'
+	wkdir2 = 'Data/Sounds/zfs'
+	train, test = load_train_test(wkdir, test_size=0.1)
+	train2, test2 = load_train_test(wkdir2, test_size=0)
 
 	model = build_model()
-	model.fit(train.data, train.labels, batch_size=10)
-	print(model.evaluate(train.data, train.labels))
-	print(model.evaluate(test.data, test.labels))
+	model.fit(train.data, train.labels, batch_size=10, epochs=1)
+	train_loss, train_acc = model.evaluate(train.data, train.labels, batch_size=10)
+	test_loss, test_acc = model.evaluate(test.data, test.labels, batch_size=10)
+	print('acc on mix train:', train_acc)
+	print('acc on mix test :', test_acc)
+	# print('on zfs test ', model.evaluate(train2.data, train2.labels, batch_size=10))
+	model.save('voice/model_state/%sRNN mix%d-%d.model' % (date_time, train_acc * 100, test_acc * 100))
