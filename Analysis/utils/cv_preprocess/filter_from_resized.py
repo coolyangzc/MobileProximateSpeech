863e3a6ef0582579b32e47d6a23567fd64969b3d
郑逢时 <962907278@qq.com>
2019-03-20 15:28:59 +0800

    [update] generalized `_remove_redundant`.

--- Analysis/utils/cv_preprocess/filter_from_resized.py
+++ Analysis/utils/cv_preprocess/filter_from_resized.py
@@ -1,30 +1,29 @@
 import os
-from tqdm import tqdm
 from utils.tools import suffix_filter, dir_filter
 
 
-def _remove_redundant(subject_dir, which='trimmed'):
+def _remove_redundant(subject_dir, target='trimmed', ref='resized'):
 	'''
 	apply filter to original/ or trimmed/, only keep jpgs exsiting in resized/
 	be careful to call this function!!
 
 	:param subject_dir:
-	:param which: 'original' or 'trimmed', depending on where the frame folders are stored
+	:param target: 'original' or 'trimmed', depending on where the frame folders are stored
 	:return del_cnt: how many deleted
 	'''
 	owd = os.getcwd()
 	os.chdir(subject_dir)
-	assert os.path.exists(which) and os.path.exists('resized')
-	os.chdir('resized')
+	assert os.path.exists(target) and os.path.exists(ref)
+	os.chdir(ref)
 	frame_folders = dir_filter(os.listdir('.'))
 	del_cnt = 0
-	for folder in tqdm(frame_folders):
+	for folder in frame_folders:
 		img_names = set(suffix_filter(os.listdir(folder), '.jpg'))
-		src_folder = os.path.join('..', which, folder)
+		trg_dir = os.path.join('..', target, folder)
 		# delete files in src_folder that are not in img_names in 'resized/'
-		for file_name in os.listdir(src_folder):
+		for file_name in os.listdir(trg_dir):
 			if file_name not in img_names:
-				file_path = os.path.join(src_folder, file_name)
+				file_path = os.path.join(trg_dir, file_name)
 				os.remove(file_path)
 				del_cnt += 1
 	os.chdir(owd)
@@ -32,8 +31,10 @@ def _remove_redundant(subject_dir, which='trimmed'):
 
 
 if __name__ == '__main__':
-	cwd = '/Users/james/MobileProximateSpeech/Analysis/Data/Study2/subjects-2/'
+	cwd = '/Volumes/TOSHIBA EXT/study2 subjects'
 	os.chdir(cwd)
-	subjects = ['hsd', 'cjr']
+	subjects = dir_filter(os.listdir('.'))
+	# print(subjects)
 	for subject in subjects:
-		print('deleted cnt:',  _remove_redundant(subject, which='trimmed'))
+		print(subject, end='\t\t')
+		print('deleted cnt:', _remove_redundant(subject, target='original', ref='resized'))
